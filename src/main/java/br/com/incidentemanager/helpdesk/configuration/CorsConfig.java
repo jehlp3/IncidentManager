dc4c63947953b9b727ea53e7e2e894c123d6bc01
JeanLima2112 <jeanlima5939@gmail.com>
2025-06-09 22:41:31 -0300

    Feat: add Cors * Config

--- src/main/java/br/com/incidentemanager/helpdesk/configuration/CorsConfig.java
+++ src/main/java/br/com/incidentemanager/helpdesk/configuration/CorsConfig.java
@@ -14,10 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
